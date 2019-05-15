d1469f0473ceb465d0a82f3ede418f373e646ce9
santiagoaguilar12 <santiago2000aguilar@gmail.com>
2019-05-14 23:53:51 -0400

    remove firebase. Setup to run locally before pushing to github

--- app.js
+++ app.js
@@ -6,47 +6,47 @@ var express     = require("express"),
     
 
 
-mongoose.connect("mongodb://localhost/help_io");
+// mongoose.connect("mongodb://localhost/help_io");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const Firestore = require('firestore');
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
+// const Firestore = require('firestore');
 
-var serviceAccount = require("./key.json");
+// var serviceAccount = require("./key.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://my-awesome-project-af874.firebaseio.com"
-});
-
-
-var db = admin.firestore();
-var sleep_yes_data = [];
-var docRef = db.collection('users').doc('Patients').collection('Anna Lovelace').doc('Graph Data').collection('Yes').doc('Sleep');
-var getDoc = docRef.get()
-    .then(doc => {
-        if (!doc.exists) {
-            console.log('No such document!');
-        } else {
-            var data = doc.data();
-
-            for (let i = 0; i < 10; i++) {
-
-                sleep_yes_data.push(Object.entries(data)[0][1][i])
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://my-awesome-project-af874.firebaseio.com"
+// });
 
 
-            }
-            console.log(sleep_yes_data);
+// var db = admin.firestore();
+// var sleep_yes_data = [];
+// var docRef = db.collection('users').doc('Patients').collection('Anna Lovelace').doc('Graph Data').collection('Yes').doc('Sleep');
+// var getDoc = docRef.get()
+//     .then(doc => {
+//         if (!doc.exists) {
+//             console.log('No such document!');
+//         } else {
+//             var data = doc.data();
+
+//             for (let i = 0; i < 10; i++) {
+
+//                 sleep_yes_data.push(Object.entries(data)[0][1][i])
 
 
-        };
+//             }
+//             console.log(sleep_yes_data);
 
-    })
-    .catch(err => {
-        console.log('Error getting document', err);
-    });
+
+//         };
+
+//     })
+//     .catch(err => {
+//         console.log('Error getting document', err);
+//     });
 
 app.use(require("express-session")(
     {
@@ -72,27 +72,32 @@ app.get("/user", function(req, res){
 });
 
 app.get("/show", function(req, res){
-    User.findById(req.params.id,function(err,foundUser){
-            res.render("show",{user:foundUser});
-        });  
+    res.render("show");
+
+    // User.findById(req.params.id,function(err,foundUser){
+
+    //     });  
 });
 
 app.get("/bio", function(req, res){
-    User.findById(req.params.id,function(err,foundUser){
-            res.render("bio",{user:foundUser});
-        });  
+    res.render("bio");
+    // User.findById(req.params.id,function(err,foundUser){
+            
+    //     });  
 });
 
 app.get("/med", function(req, res){
-    User.findById(req.params.id,function(err,foundUser){
-            res.render("med",{user:foundUser});
-        });  
+    res.render("med");
+    // User.findById(req.params.id,function(err,foundUser){
+           
+    //     });  
 });
 
 app.get("/history", function(req, res){
-    User.findById(req.params.id,function(err,foundUser){
-            res.render("history",{user:foundUser});
-        });  
+    res.render("history");
+    // User.findById(req.params.id,function(err,foundUser){
+            
+    //     });  
 });
 
 app.get("/graphs", function (req, res) {
